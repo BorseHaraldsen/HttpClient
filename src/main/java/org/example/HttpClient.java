35575b28a654928cb54847171778ce43a7c3736e
BorseHaraldsen <Augustborse@gmail.com>
2023-09-11 12:58:32 +0200

    First code

---
+++ src/main/java/org/example/HttpClient.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class HttpClient {
+
+
+
+    public HttpClient(String host, int port, String url) {
+        try (Socket socket = new Socket(host, port)){
+
+            String request = String.format("GET %s HTTP/1.1\r\nHost: %s \r\n\r\n", url, host);
+
+            socket.getOutputStream().write(request.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
